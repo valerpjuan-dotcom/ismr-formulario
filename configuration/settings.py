b9f7c35478dc6cdc86da6a3c1d444f1f2eff009b
valerpjuan-dotcom <valerpjuan@gmail.com>
2026-02-23 10:11:21 -0500

    Update settings.py

--- configuration/settings.py
+++ configuration/settings.py
@@ -1,10 +1,22 @@
 defaults = {
     "autenticado": False, "username": None, "nombre_completo": None,
     "debe_cambiar_password": False, "es_admin": False, "vista": None,
-    "hechos": [], "perfiles": [], "antecedentes": []
+    "hechos": [], "perfiles": [], "antecedentes": [], "perfiles_actuales": []
 }
 
 TAB_NOMBRES = {
-    "individual": {"casos": "Individual",  "hechos": "Hechos_Individual",  "perfiles": "Perfiles_Individual",  "antecedentes": "Antecedentes_Individual"},
-    "colectivo":  {"casos": "Colectivo",   "hechos": "Hechos_Colectivo",   "perfiles": "Perfiles_Colectivo",   "antecedentes": "Antecedentes_Colectivo"},
+    "individual": {
+        "casos": "Individual",
+        "hechos": "Hechos_Individual",
+        "perfiles": "Perfiles_Individual",
+        "antecedentes": "Antecedentes_Individual",
+        "perfiles_actuales": "PerfilesActuales_Individual",
+    },
+    "colectivo": {
+        "casos": "Colectivo",
+        "hechos": "Hechos_Colectivo",
+        "perfiles": "Perfiles_Colectivo",
+        "antecedentes": "Antecedentes_Colectivo",
+        "perfiles_actuales": "PerfilesActuales_Colectivo",
+    },
 }
